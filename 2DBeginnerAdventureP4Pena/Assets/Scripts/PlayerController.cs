a5343f96f0e7dbdb3d25c471c6c5101ca385cfff
mstyyyyy <722833@student.dusd.net>
2024-11-12 11:43:38 -0800

    Animations for ducko and enemy
    I made a animation for ducko and enemy in each animator.

--- 2DBeginnerAdventureP4Pena/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP4Pena/Assets/Scripts/PlayerController.cs
@@ -18,12 +18,15 @@ public class PlayerController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,6 +34,14 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+
+        }
+
 
         if(isInvincible)
         {
