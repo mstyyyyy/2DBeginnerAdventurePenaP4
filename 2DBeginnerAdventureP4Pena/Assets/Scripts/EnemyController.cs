2ec917d8113a503f3665aede39d038080919ac61
mstyyyyy <722833@student.dusd.net>
2024-11-18 11:56:51 -0800

    World interactions
    I made a interaction script to shoot projectiles. I am also working on another animation for my robot.

--- 2DBeginnerAdventureP4Pena/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP4Pena/Assets/Scripts/EnemyController.cs
@@ -10,6 +10,8 @@ public class EnemyController : MonoBehaviour
 
     Rigidbody2D rigidbody2d;
 
+    bool broken;
+
     float timer;
     int direction = 1;
 
@@ -26,6 +28,11 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
         if (timer < 0)
         {
@@ -35,6 +42,11 @@ public class EnemyController : MonoBehaviour
     }
    void FixedUpdate()
    {
+        if (!broken)
+        {
+            return;
+
+        }
        Vector2 position = rigidbody2d.position;
        if(vertical)
        {
@@ -61,5 +73,11 @@ public class EnemyController : MonoBehaviour
             player.ChangeHealth(-1);
 
         }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2d.simulated = false;
     }
 }
--- 2DBeginnerAdventureP4Pena/Assets/Scripts/Projectile.cs
+++ 2DBeginnerAdventureP4Pena/Assets/Scripts/Projectile.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
@@ -14,7 +14,10 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.magnitude > 100.0f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void Launch(Vector2 direction, float force)
@@ -24,7 +27,12 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("Projectile Collision with " + other.gameObject);
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+
         Destroy(gameObject);
     }
 }
